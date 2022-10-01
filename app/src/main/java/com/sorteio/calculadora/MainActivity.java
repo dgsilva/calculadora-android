af24d7bdb4c1287856cb8b9d910e590b54a78083
dgsilva <diegorj93@gmail.com>
2022-10-01 14:55:19 -0300

    implementação do botão subtração

--- app/src/main/java/com/sorteio/calculadora/MainActivity.java
+++ app/src/main/java/com/sorteio/calculadora/MainActivity.java
@@ -39,4 +39,28 @@ public class MainActivity extends AppCompatActivity {
        }
 
     }
+
+
+    public void subtrair(View view){
+        EditText n1 = (EditText) findViewById(R.id.txtN1);
+        EditText n2 = (EditText) findViewById(R.id.txtN2);
+        String v1 = n1.getText().toString();
+        String v2 = n2.getText().toString();
+        if((v1.trim().isEmpty())||(v2.trim().isEmpty())){
+            AlertDialog.Builder dlg =  new AlertDialog.Builder(this);
+            dlg.setMessage("Há dados em Branco");
+            dlg.setNeutralButton("Ok",null);
+            dlg.show();
+        }else {
+            int num1 = Integer.parseInt(n1.getText().toString());
+            int num2 = Integer.parseInt(n2.getText().toString());
+            int sub = (num1 - num2);
+            AlertDialog.Builder dlg =  new AlertDialog.Builder(this);
+            dlg.setMessage("O resultado da subtração foi: " + sub);
+            dlg.setNeutralButton("Ok", null);
+            dlg.show();
+            Toast.makeText(this, "Resultado: ", +5000+ sub + Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
